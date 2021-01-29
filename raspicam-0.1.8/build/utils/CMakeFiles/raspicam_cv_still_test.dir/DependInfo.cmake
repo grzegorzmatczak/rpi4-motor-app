bb5e5e9985127a7f745068e3846b66c4e47bf007
Grzegorz Matczak <grzegorz.matczak@unikie.com>
2021-01-29 15:04:56 +0100

    Change raspicam version

---
+++ raspicam-0.1.8/build/utils/CMakeFiles/raspicam_cv_still_test.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gmatczak/raspicam-0.1.8/utils/raspicam_cv_still_test.cpp" "/home/gmatczak/raspicam-0.1.8/build/utils/CMakeFiles/raspicam_cv_still_test.dir/raspicam_cv_still_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../utils/../dependencies/interface/vcos"
+  "../utils/../dependencies/interface"
+  "../utils/../dependencies"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gmatczak/raspicam-0.1.8/build/src/CMakeFiles/raspicam_cv.dir/DependInfo.cmake"
+  "/home/gmatczak/raspicam-0.1.8/build/src/CMakeFiles/raspicam.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
